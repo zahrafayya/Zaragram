9bd053e0a38c44b32eb7690dc7ec017a08e84eb7
zahrafayya <zahra.fayyadiyati@gmail.com>
2022-07-19 22:13:29 +0700

    feat: add image (profile picture) field on profile class

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
@@ -46,7 +47,19 @@ class ProfilesController extends Controller
         ]);
 
         $obj = User::findOrFail($user)->first();
-        auth()->$obj->profile->update($data);
+
+        if (request('image')) // do something special if the request have 'image'
+        {
+            $imagePath = request('image')->store('profpics', 'public');
+
+            $image = Image::make(public_path("storage/{$imagePath}"))->fit(1000, 1000);
+            $image->save();
+        }
+
+        auth()->$obj->profile->update(array_merge(
+            $data,
+            ['image' => $imagePath],
+        ));
 
         return redirect('/profile/' . auth()->user()->username);
     }
--- database/migrations/2022_07_08_151003_create_profiles_table.php
+++ database/migrations/2022_07_08_151003_create_profiles_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('profiles', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->string('image')->nullable();
             $table->string('title')->nullable();
             $table->text('description')->nullable();
             $table->string('url')->nullable();
